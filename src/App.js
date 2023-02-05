d43666e0bc06e8469768c25037155dafa9ec9dc6
appuser <appuser@journey2-7655956f4c-fct6n>
2023-02-05 12:55:51 +0000

    starter project

---
+++ src/App.js
@@ -0,0 +1,57 @@
+function App() {
+  const ingredients = [
+    {
+      name: "envelopes unflavored gelatine",
+      quantity: "2"
+    },
+    {
+      name: "cup sugar",
+      quantity: "1/2"
+    },
+    {
+      name: "teaspoon salt",
+      quantity: "1"
+    },
+    {
+      name: "can (12 oz) apple juice",
+      quantity: "1"
+    },
+    {
+      name: " cup lemon juice",
+      quantity: "1/2"
+    },
+    {
+      name: "tablespoons vinegar",
+      quantity: "2"
+    },
+    {
+      name: "cup shredded carrot",
+      quantity: "1"
+    },
+    {
+      name: "cup sliced celery",
+      quantity: "1"
+    },
+    {
+      name: "cup finely shredded cabbage",
+      quantity: "1"
+    },
+    {
+      name: "cup chopped green pepper",
+      quantity: "1/2"
+    },
+    {
+      name: "can (4 oz) chopped pimiento",
+      quantity: "1"
+    }
+  ]
+  console.log(ingredients)
+  
+  return (
+    <div>
+        Grandma's recipes
+    </div>
+  );
+}
+
+export default App;
