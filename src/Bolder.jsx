de302b228fab9efec18fdbaec1d632600ac7fbe4
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-06 20:32:41 +0100

    Added function that changes ingrrdients in literals to bold

---
+++ src/Bolder.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Bolder = (str) => {
+    var boldedText = "lol";
+    const wordsToBold = [
+        'gelatin', 'sugar', 'salt', 'apple juice', 'lemon juice', 'vinegar', 'carrot', 'celery', 'cabbage', 'green pepper', 'pimiento', 'water'
+    ];
+    if (str.length > 3) {
+        boldedText = str;
+    } else {
+        return str;
+    }
+    for (let boldWord of wordsToBold) {
+        boldedText = boldedText.replace(boldWord, `<strong>${boldWord}</strong>`);
+    }
+    return (
+        <p dangerouslySetInnerHTML={{__html: boldedText}}/>);
+}
+export default Bolder;
