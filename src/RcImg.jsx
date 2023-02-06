50b16fc5a357abfd256368dfb6d609c9b3fb9faa
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-06 20:30:03 +0100

    Added header and image

---
+++ src/RcImg.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import presentation from "../src/images/perfection_salad.jpg";
+function RcImg() {
+    return (
+        <img className="presentation" alt="food presentation"
+             src={presentation} />
+    );
+}
+export default RcImg;
---
+++ src/RcName.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function RcName() {
+    return (
+            <h1 style={{textAlign: "center", paddingRight: "39rem", marginTop: "1rem", fontFamily: "Sacramento", fontSize: "3.14159rem", color: "lightgreen"}}>
+                Perfection Salad
+            </h1>
+    );
+}
+export default RcName;
